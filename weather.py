c054cc62ddddd58e558a9c3328eb772fd1423353
Matt <Matthew.White02@gmail.com>
2021-08-19 07:49:34 +0100

    Add weather script

---
+++ weather.py
@@ -0,0 +1,11 @@
+temp = 35
+
+if temp > 30:
+	print("It's too hot!")
+	print("Stay inside!")
+elif temp < 15:
+	print("It's too cold!")
+	print("Stay inside!")
+else:
+	print("Enjoy the weather!")
+print("Have a great day!")
